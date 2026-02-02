46c22cb4d1dd93d1608c44c0ac5f2f8aea514264
Arif Raza <arif3raza@gmail.com>
2026-02-02 18:06:27 +0530

    new code

--- firstprogram.py
+++ firstprogram.py
@@ -1,4 +1,34 @@
 print("today i am starting python from scratch")
 
 print("true")
-print('hello',1,4.5,true)
+
+
+
+name= 'arif'
+print(name)
+
+a=5
+b=6
+print(a+b)
+
+# dynamic binding
+a=5
+print(a)
+a='arif'
+print(a)
+
+a,b,c=1,2,3
+print(a,b,c)
+#same value
+a=b=c=5
+print(a,b,c)
+#keywords
+print(print)
+#get confused
+
+#identifier
+#cannot start with digit
+first_name='arif'
+print(first_name)
+#cannot use special symbol
+#identifiers cannot be keywords
