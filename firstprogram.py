f76946ee19c536165c5ac3deebe30ad1127bb3cb
Arif Raza <arif3raza@gmail.com>
2026-02-03 21:17:25 +0530

    new learn

--- firstprogram.py
+++ firstprogram.py
@@ -32,3 +32,36 @@ first_name='arif'
 print(first_name)
 #cannot use special symbol
 #identifiers cannot be keywords
+
+#input 
+input("name:")
+#add two number by giving  output
+fnum = int(input('enter first number:'))
+snum = int(input('enter second number:'))  
+sum = fnum + snum
+print("sum",sum)
+
+#literals
+a = 0b1010 #binary
+b = 100 #decimal 
+c = 0o310 #octa
+d = 0x12c #hexa
+
+print(a,b,c,d)
+
+#float
+float_1 = 10.5
+float_2 = 1.5e2
+float_3 = 1.5e-3
+
+print(float_1, float_2, float_3)
+
+#complex
+
+x= 3.14j
+print(x,x.imag,x.real)
+
+#string
+string = "this is python"
+print(string)
+
