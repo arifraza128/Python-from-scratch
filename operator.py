71b85c9827d70358b87dd8dbcfa863654f7026f2
Arif Raza <arif3raza@gmail.com>
2026-02-05 21:19:39 +0530

    Operators

---
+++ operator.py
@@ -0,0 +1,41 @@
+#arithemetic operator
+
+print(1+2)
+print(5*6)
+print(5**5) #5 power of 5
+#relational op
+
+print(5>6)
+print(5<6)
+print(5==5)
+print(4!=4)
+#logical operator
+
+print(1 and 0)
+print(1 or 0)
+print(not 1)
+#bitwise operator
+
+#bitwise and
+print(2 & 3)
+#bitwise or
+print(2 | 3)
+#bitwise exor
+print(2 ^ 3)
+#bitwise not
+print(~2)
+
+print(4 >> 2)
+print(5 << 3)
+
+# assignment operator
+
+a=5
+a+=2
+print(a)
+
+# membership Operator
+
+#in/not in
+print('D' in 'delhi')
+print(1 in [1,2,3,4])
