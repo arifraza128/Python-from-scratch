7ed46dab80001598bc49ce122e53068756e689e9
Arif Raza <arif3raza@gmail.com>
2026-02-06 21:51:53 +0530

    loop

--- loop.py
+++ loop.py
@@ -21,3 +21,22 @@ print(datetime.datetime.now())
 
 help('modules')
 
+# loops in python
+
+#while loop
+
+num = int(input("Enter a number: "))
+i=1
+while i<10:
+    print(num*i)
+    print(num ,'*' ,i ,'=' ,num*i)
+    i+=1
+
+ # while loop with else
+x = 1
+while x < 3:
+  print(x)
+  x += 1
+else:
+  print('limit crossed')
+        
