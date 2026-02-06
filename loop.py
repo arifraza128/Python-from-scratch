3adb5c263d696e9b72b02d35b5507239709e3e9a
Arif Raza <arif3raza@gmail.com>
2026-02-06 21:49:36 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,23 @@
+#module
+#module
+import math
+
+math.factorial(10)
+
+# keyword
+
+import keyword
+print(keyword.kwlist)
+
+# random
+
+import random
+print(random.randint(1, 10))
+
+# datetime
+
+import datetime
+print(datetime.datetime.now())
+
+help('modules')
+
