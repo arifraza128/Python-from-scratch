d90f9107824bd9796c4a9cace9f78bc8f1f05068
Arif Raza <arif3raza@gmail.com>
2026-04-09 19:21:20 +0530

    Update guess_number.py

--- guess-the-number/guess_number.py
+++ guess-the-number/guess_number.py
@@ -7,3 +7,9 @@ print("Guess a number btw 1 and 20")
 
 while guess != number:
   guess=int(input("Enter your Guess: "))
+  if guess < number:
+        print("Too low! Try again.")
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You guessed it right ")
