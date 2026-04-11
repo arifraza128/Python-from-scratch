c842912a1da7a9c413e763b0e15005694835f8ae
Arif Raza <arif3raza@gmail.com>
2026-04-11 19:47:27 +0530

    Change do-while loop to while loop for number input
    Refactor loop structure to use a while loop for input.

--- guess-the-number/do-while.py
+++ guess-the-number/do-while.py
@@ -6,3 +6,16 @@ while True:
     
     if num == 0:
         break
+
+
+
+#condition
+
+num = 0
+
+while True:
+    num = int(input("Enter a number (0 to stop): "))
+    print("You entered:", num)
+    
+    if num == 0:
+        break
