67e6dc9839128c336d5e9b5f883bf1aa25f13461
Arif Raza <arif3raza@gmail.com>
2026-04-23 19:14:13 +0530

    Implement binary search algorithm

--- Recursion/Binary-Search.py
+++ Recursion/Binary-Search.py
@@ -1,1 +1,15 @@
+def binary_search(arr, left, right, target):
+    if left > right:
+        return -1
 
+    mid = (left + right) // 2
+
+    if arr[mid] == target:
+        return mid
+    elif target < arr[mid]:
+        return binary_search(arr, left, mid - 1, target)
+    else:
+        return binary_search(arr, mid + 1, right, target)
+
+arr = [1, 2, 3, 4, 5, 6]
+print(binary_search(arr, 0, len(arr)-1, 4))
