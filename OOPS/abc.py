b399d64c1697337bfd58a26a07af7dae33d57be6
Arif Raza <arif3raza@gmail.com>
2026-04-29 21:17:05 +0530

    Add Shape class hierarchy with area calculations
    Implemented abstract base class for shapes and defined area methods for Rectangle and Circle.

--- OOPS/abc.py
+++ OOPS/abc.py
@@ -1,1 +1,31 @@
+from abc import ABC, abstractmethod
 
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def area(self):
+        return self.l * self.w
+
+
+class Circle(Shape):
+    def __init__(self, r):
+        self.r = r
+
+    def area(self):
+        return 3.14 * self.r * self.r
+
+
+r = Rectangle(4, 5)
+c = Circle(3)
+
+print(r.area())
+print(c.area())
