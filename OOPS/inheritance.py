d5778810dabe7ba0c04122eb733bf25778a93880
Arif Raza <arif3raza@gmail.com>
2026-04-30 08:42:19 +0530

    Implement inheritance with Person, Student, and Teacher classes

--- OOPS/inheritance.py
+++ OOPS/inheritance.py
@@ -1,1 +1,38 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    def show_details(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+
+class Student(Person):
+    def __init__(self, name, age, course):
+        super().__init__(name, age)
+        self.course = course
+
+    def show_student(self):
+        print("Course:", self.course)
+
+
+class Teacher(Person):
+    def __init__(self, name, age, subject):
+        super().__init__(name, age)
+        self.subject = subject
+
+    def show_teacher(self):
+        print("Subject:", self.subject)
+
+
+s1 = Student("Arif", 21, "Cloud Computing")
+t1 = Teacher("Rahul", 35, "Mathematics")
+
+print("Student Info:")
+s1.show_details()
+s1.show_student()
+
+print("\nTeacher Info:")
+t1.show_details()
+t1.show_teacher()
