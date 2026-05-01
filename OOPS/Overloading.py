7f31f723762f1a27899a1313bf4f5e0405161ea5
Arif Raza <arif3raza@gmail.com>
2026-05-01 22:07:14 +0530

    Add Calculator class with overloaded add method

--- OOPS/Overloading.py
+++ OOPS/Overloading.py
@@ -1,1 +1,11 @@
+class Calculator:
+    def add(self, a, b, c=None):
+        if c is None:
+            return a + b
+        else:
+            return a + b + c
 
+calc = Calculator()
+
+print("Sum of 2 numbers:", calc.add(5, 10))
+print("Sum of 3 numbers:", calc.add(1, 2, 3))
