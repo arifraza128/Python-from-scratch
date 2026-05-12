750594314b403b62ab5fa7ec778a717f37b6dc7d
Arif Raza <arif3raza@gmail.com>
2026-05-12 19:00:50 +0530

    Update greatestelement.py

--- list/greatestelement.py
+++ list/greatestelement.py
@@ -1,1 +1,9 @@
-
+l=[12,22,33,77,128,90]
+largestNumber = l[0]
+index = 0
+for i in range (len(l)):
+    if l[i]>largestNumber:
+        largestNumber=l[i]
+        index=i
+print("the largest number is :",largestNumber)
+print("The index is :",index)
