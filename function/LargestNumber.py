d6daea43878bf38a45659af1175573978ccb89c3
Arif Raza <arif3raza@gmail.com>
2026-05-16 17:28:02 +0530

    Update LargestNumber.py

--- function/LargestNumber.py
+++ function/LargestNumber.py
@@ -1,1 +1,13 @@
+def find_largest(numbers):
+    largest = numbers[0]
 
+    for num in numbers:
+        if num > largest:
+            largest = num
+
+    return largest
+
+nums = [3, 7, 2, 9, 5]
+result = find_largest(nums)
+
+print("Largest number is:", result)
