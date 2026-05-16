647c1b10daa3d04d0448cbbddab6fb21e4b4d9f5
Arif Raza <arif3raza@gmail.com>
2026-05-16 17:30:15 +0530

    Add function to check if a number is prime

--- function/Prime.py
+++ function/Prime.py
@@ -1,1 +1,17 @@
+def is_prime(number):
+    if number <= 1:
+        return False
 
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+num = 7
+
+if is_prime(num):
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
