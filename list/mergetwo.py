e11af7eaaca73bdb8e1e93e3e5ea74ae3c2bc299
Arif Raza <arif3raza@gmail.com>
2026-05-17 18:44:30 +0530

    Implement merging of two sorted lists
    Merge two sorted lists into one sorted list.

--- list/mergetwo.py
+++ list/mergetwo.py
@@ -1,1 +1,24 @@
+a = [1, 3, 5]
+b = [2, 4, 6]
 
+result = []
+
+i = 0
+j = 0
+
+while i < len(a) and j < len(b):
+    if a[i] < b[j]:
+        result.append(a[i])
+        i += 1
+    else:
+        result.append(b[j])
+        j += 1
+
+while i < len(a):
+    result.append(a[i])
+    i += 1
+while j < len(b):
+    result.append(b[j])
+    j += 1
+
+print(result)
