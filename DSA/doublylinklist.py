76f2965a5ae22cdc75f4fbe37bf7f62746ad33ab
Arif Raza <arif3raza@gmail.com>
2026-06-11 21:05:39 +0530

    Add doubly linked list implementation
    Implement a doubly linked list with basic node structure and traversal.

--- DSA/doublylinklist.py
+++ DSA/doublylinklist.py
@@ -1,1 +1,20 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
 
+head = Node(10)
+second = Node(20)
+third = Node(30)
+
+head.next = second
+second.prev = head
+second.next = third
+third.prev = second
+
+temp = head
+while temp:
+    print(temp.data, end=" <-> ")
+    temp = temp.next
+print("None")
