879bc2de250b76f348f624d79e1e9b1f3b26c139
Arif Raza <arif3raza@gmail.com>
2026-06-12 20:01:32 +0530

    Implement Circular Singly Linked List in Python

--- DSA/CircularSinglyLinkedList.py
+++ DSA/CircularSinglyLinkedList.py
@@ -1,1 +1,23 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+head = Node(10)
+second = Node(20)
+third = Node(30)
+
+head.next = second
+second.next = third
+third.next = head
+
+temp = head
+
+while True:
+    print(temp.data, end=" -> ")
+    temp = temp.next
+
+    if temp == head:
+        break
+
+print("(back to head)")
