d4b9591a22f3154cfbf483d371283ac67699e005
Arif Raza <arif3raza@gmail.com>
2026-06-20 20:09:16 +0530

    Update ValidParentheses.py

--- DSA/ValidParentheses.py
+++ DSA/ValidParentheses.py
@@ -1,1 +1,17 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        mp = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
 
+        for ch in s:
+            if ch in "([{":
+                stack.append(ch)
+            else:
+                if not stack or stack.pop() != mp[ch]:
+                    return False
+
+        return len(stack) == 0
