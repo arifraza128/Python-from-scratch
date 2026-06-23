342fb55cf548480ef7c37c7efc4f8131173787ab
Arif Raza <arif3raza@gmail.com>
2026-06-23 21:15:38 +0530

    Implement sum of subarray minimums function

--- DSA/SumofSubarrayMinimums.py
+++ DSA/SumofSubarrayMinimums.py
@@ -1,1 +1,35 @@
+class Solution:
+    def sumSubarrayMins(self, arr):
+        MOD = 10**9 + 7
+        n = len(arr)
 
+        left = [0] * n
+        right = [0] * n
+        stack = []
+
+        for i in range(n):
+            count = 1
+
+            while stack and stack[-1][0] > arr[i]:
+                count += stack.pop()[1]
+
+            left[i] = count
+            stack.append((arr[i], count))
+
+        stack = []
+
+        for i in range(n - 1, -1, -1):
+            count = 1
+
+            while stack and stack[-1][0] >= arr[i]:
+                count += stack.pop()[1]
+
+            right[i] = count
+            stack.append((arr[i], count))
+
+        ans = 0
+
+        for i in range(n):
+            ans = (ans + arr[i] * left[i] * right[i]) % MOD
+
+        return ans
