4aaca757768cd2b4ac535a43328dab87b0a4c6e1
Arif Raza <arif3raza@gmail.com>
2026-07-07 16:33:05 +0530

    Update KFrequentElements.py

--- DSA/KFrequentElements.py
+++ DSA/KFrequentElements.py
@@ -1,1 +1,19 @@
+from collections import Counter
 
+class Solution:
+    def topKFrequent(self, nums, k):
+        count = Counter(nums)
+
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in count.items():
+            buckets[freq].append(num)
+
+        ans = []
+
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                ans.append(num)
+
+                if len(ans) == k:
+                    return ans
