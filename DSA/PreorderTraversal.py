92a9bdfec4f59a4edacecf821d04e7ad33593fc9
Arif Raza <arif3raza@gmail.com>
2026-07-10 09:18:45 +0530

    Update PreorderTraversal.py

--- DSA/PreorderTraversal.py
+++ DSA/PreorderTraversal.py
@@ -1,1 +1,14 @@
+class Solution:
+    def preorderTraversal(self, root):
+        ans = []
 
+        def dfs(node):
+            if not node:
+                return
+
+            ans.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ans
