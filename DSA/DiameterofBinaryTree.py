453585b43f13d7976ec204208f4374b53029324c
Arif Raza <arif3raza@gmail.com>
2026-07-20 18:10:02 +0530

    Update DiameterofBinaryTree.py

--- DSA/DiameterofBinaryTree.py
+++ DSA/DiameterofBinaryTree.py
@@ -1,1 +1,18 @@
+class Solution:
+    def diameterOfBinaryTree(self, root):
+        self.ans = 0
 
+        def dfs(node):
+            if not node:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.ans = max(self.ans, left + right)
+
+            return 1 + max(left, right)
+
+        dfs(root)
+
+        return self.ans
