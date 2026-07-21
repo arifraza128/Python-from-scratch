a2a22e626e8a1e7fecb23e89e8d06674b5971a47
Arif Raza <arif3raza@gmail.com>
2026-07-21 18:12:07 +0530

    Implement search in a Binary Search Tree

--- DSA/search.py
+++ DSA/search.py
@@ -1,1 +1,9 @@
+class Solution:
+    def searchBST(self, root, val):
+        if not root or root.val == val:
+            return root
 
+        if val < root.val:
+            return self.searchBST(root.left, val)
+
+        return self.searchBST(root.right, val)
