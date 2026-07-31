c1b55fcde587fe8c93b514462b9023b91566830d
Arif Raza <arif3raza@gmail.com>
2026-07-31 19:40:06 +0530

    Implement range sum calculation for BST

--- DSA/RangeSumofBST.py
+++ DSA/RangeSumofBST.py
@@ -1,1 +1,16 @@
+class Solution:
+    def rangeSumBST(self, root, low, high):
+        if not root:
+            return 0
 
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+
+        if root.val > high:
+            return self.rangeSumBST(root.left, low, high)
+
+        return (
+            root.val
+            + self.rangeSumBST(root.left, low, high)
+            + self.rangeSumBST(root.right, low, high)
+        )
