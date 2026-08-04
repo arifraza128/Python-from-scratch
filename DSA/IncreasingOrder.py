20ca6833a3e0c8e163517dd19fdfafa1731d2ce7
Arif Raza <arif3raza@gmail.com>
2026-08-04 17:52:43 +0530

    Update IncreasingOrder.py

--- DSA/IncreasingOrder.py
+++ DSA/IncreasingOrder.py
@@ -1,1 +1,20 @@
+class Solution:
+    def increasingBST(self, root):
+        dummy = TreeNode(0)
+        self.curr = dummy
 
+        def inorder(node):
+            if not node:
+                return
+
+            inorder(node.left)
+
+            node.left = None
+            self.curr.right = node
+            self.curr = node
+
+            inorder(node.right)
+
+        inorder(root)
+
+        return dummy.right
