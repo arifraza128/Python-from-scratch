cfea75d88a7b76e89142d641b87e2477a717ca5e
Arif Raza <arif3raza@gmail.com>
2026-08-07 20:38:38 +0530

    Update taskschedular.py

--- DSA/taskschedular.py
+++ DSA/taskschedular.py
@@ -1,1 +1,10 @@
+from collections import Counter
 
+class Solution:
+    def leastInterval(self, tasks, n):
+        freq = Counter(tasks).values()
+
+        maxFreq = max(freq)
+        count = list(freq).count(maxFreq)
+
+        return max(len(tasks), (maxFreq - 1) * (n + 1) + count)
