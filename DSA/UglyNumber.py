b9c630c4f29731a89191a6150b4d0045a30cc371
Arif Raza <arif3raza@gmail.com>
2026-08-08 12:45:39 +0530

    Implement nthUglyNumber function to find nth ugly number

--- DSA/UglyNumber.py
+++ DSA/UglyNumber.py
@@ -1,1 +1,25 @@
+class Solution:
+    def nthUglyNumber(self, n):
+        ugly = [1]
 
+        i2 = i3 = i5 = 0
+
+        while len(ugly) < n:
+            nextNum = min(
+                ugly[i2] * 2,
+                ugly[i3] * 3,
+                ugly[i5] * 5
+            )
+
+            ugly.append(nextNum)
+
+            if nextNum == ugly[i2] * 2:
+                i2 += 1
+
+            if nextNum == ugly[i3] * 3:
+                i3 += 1
+
+            if nextNum == ugly[i5] * 5:
+                i5 += 1
+
+        return ugly[-1]
