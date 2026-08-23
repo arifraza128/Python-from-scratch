2a4ccb51b66f73db2a5280cb69bd8274522ee57f
Arif Raza <arif3raza@gmail.com>
2026-08-23 19:19:56 +0530

    Update WordLadder.py

--- DSA/WordLadder.py
+++ DSA/WordLadder.py
@@ -1,1 +1,26 @@
+from collections import deque
 
+class Solution:
+    def ladderLength(self, beginWord, endWord, wordList):
+        words = set(wordList)
+
+        if endWord not in words:
+            return 0
+
+        q = deque([(beginWord, 1)])
+
+        while q:
+            word, steps = q.popleft()
+
+            if word == endWord:
+                return steps
+
+            for i in range(len(word)):
+                for ch in "abcdefghijklmnopqrstuvwxyz":
+                    new_word = word[:i] + ch + word[i + 1:]
+
+                    if new_word in words:
+                        words.remove(new_word)
+                        q.append((new_word, steps + 1))
+
+        return 0
